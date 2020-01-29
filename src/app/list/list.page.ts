e4d768e87ac9d144ec0aef94c637f772734a5e72
Thomas Meyer <thomas.meyer@aa.com>
2020-01-29 15:51:34 -0600

    added search functionality

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -8,9 +8,16 @@ import { DatabaseService } from '../database/database.service';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
+
   reverseType: Boolean = false;
+  selectType: Boolean = false;
   reverseName: Boolean = false;
+  selectName: Boolean = false;
   reverseCategory: Boolean = false;
+  selectCategory: Boolean = false;
+
+  searchValue: string = '';
+
   private icons = [
     'flask',
     'wifi',
@@ -25,6 +32,7 @@ export class ListPage implements OnInit {
   ];
   
   public exerciseList: Array<Exercise>;
+  public list: Array<Exercise>;
   constructor(
     public dbService: DatabaseService,
   ) { }
@@ -37,42 +45,80 @@ export class ListPage implements OnInit {
   public getExercises() {
     this.dbService.selectFromTable(`*`, `exercises`).then((res) => {
       this.exerciseList = res as Array<Exercise>;
+      this.list = this.exerciseList;
       this.listByName();
     }).catch((e) => {
       console.log(e);
     })
   }
 
+  searchExercises(event) {
+    console.log(`%c Searching...`, 'color: green');
+    console.log(this.searchValue);
+    try {
+      if (this.selectName) {
+        this.exerciseList = this.list.filter((item) => { return item.exerciseName.toLowerCase().includes(this.searchValue.toLowerCase())});
+      } else if (this.selectType) {
+        this.exerciseList = this.list.filter((item) => { return item.exerciseType.toLowerCase().includes(this.searchValue.toLowerCase())});
+      } else {
+        this.exerciseList = this.list.filter((item) => { return item.exerciseCategory.toLowerCase().includes(this.searchValue.toLowerCase())});
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   listByName() {
-    console.log(`%c By Name`, 'color: green; font-weight: bold');
-    if (!this.reverseName) {
-      this.exerciseList.sort((a, b) => (a.exerciseName > b.exerciseName) ? 1 : -1);
-      this.reverseName = true;
-    } else {
-      this.exerciseList.sort((a, b) => (a.exerciseName < b.exerciseName) ? 1 : -1);
-      this.reverseName = false;
+    try {
+      this.selectName = true;
+      this.selectCategory = false;
+      this.selectType = false;
+      console.log(`%c By Name`, 'color: green; font-weight: bold');
+      if (!this.reverseName) {
+        this.exerciseList.sort((a, b) => (a.exerciseName > b.exerciseName) ? 1 : -1);
+        this.reverseName = true;
+      } else {
+        this.exerciseList.sort((a, b) => (a.exerciseName < b.exerciseName) ? 1 : -1);
+        this.reverseName = false;
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 
   listByType() {
-    console.log(`%c By Type`, 'color: yellow; font-weight: bold');
-    if (!this.reverseType) {
-      this.exerciseList.sort((a, b) => (a.exerciseType > b.exerciseType) ? 1 : -1);
-      this.reverseType = true;
-    } else {
-      this.exerciseList.sort((a, b) => (a.exerciseType < b.exerciseType) ? 1 : -1);
-      this.reverseType = false;
+    try {
+      this.selectName = false;
+      this.selectCategory = false;
+      this.selectType = true;
+      console.log(`%c By Type`, 'color: yellow; font-weight: bold');
+      if (!this.reverseType) {
+        this.exerciseList.sort((a, b) => (a.exerciseType > b.exerciseType) ? 1 : -1);
+        this.reverseType = true;
+      } else {
+        this.exerciseList.sort((a, b) => (a.exerciseType < b.exerciseType) ? 1 : -1);
+        this.reverseType = false;
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 
   listByCategory() {
-    console.log(`%c By Category`, 'color: red; font-weight: bold');
-    if (!this.reverseCategory) {
-      this.exerciseList.sort((a, b) => (a.exerciseCategory > b.exerciseCategory) ? 1 : -1);
-      this.reverseCategory = true;
-    } else {
-      this.exerciseList.sort((a, b) => (a.exerciseCategory < b.exerciseCategory) ? 1 : -1);
-      this.reverseCategory = false;
+    try {
+      this.selectName = false;
+      this.selectCategory = true;
+      this.selectType = false;
+      console.log(`%c By Category`, 'color: red; font-weight: bold');
+      if (!this.reverseCategory) {
+        this.exerciseList.sort((a, b) => (a.exerciseCategory > b.exerciseCategory) ? 1 : -1);
+        this.reverseCategory = true;
+      } else {
+        this.exerciseList.sort((a, b) => (a.exerciseCategory < b.exerciseCategory) ? 1 : -1);
+        this.reverseCategory = false;
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
   // add back when alpha.4 is out
